cc6fff7e267828882ddfb82f1c7efa7e550f70e1
GiorgiOrmotsadze <giorgiormotsadze40@gmail.com>
2025-01-22 14:52:40 +0400

    initial commit

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,48 @@
 
 import Foundation
 
+struct StoryBrain {
+    var storyNumber = 0
+
+    let stories = [
+        Story(
+            title: "You see a fork in the road.",
+            choice1: "Take a left.", choice1Destination: 1,
+            choice2: "Take a right.", choice2Destination: 2
+        ),
+        Story(
+            title: "You see a river.",
+            choice1: "Swim across.", choice1Destination: 3,
+            choice2: "Build a raft.", choice2Destination: 3
+        ),
+        Story(
+            title: "You meet a wise old man.",
+            choice1: "Listen to his story.", choice1Destination: 0,
+            choice2: "Ignore him.", choice2Destination: 0
+        )
+    ]
+
+    func getStoryTitle() -> String {
+        return stories[storyNumber].title
+    }
+
+    func getChoice1() -> String {
+        return stories[storyNumber].choice1
+    }
+
+    func getChoice2() -> String {
+        return stories[storyNumber].choice2
+    }
+
+    mutating func nextStory(userChoice: String) {
+        if userChoice == stories[storyNumber].choice1 {
+            storyNumber = stories[storyNumber].choice1Destination
+        } else {
+            storyNumber = stories[storyNumber].choice2Destination
+        }
+        if storyNumber >= stories.count {
+            storyNumber = 0
+        }
+    }
+}
 
